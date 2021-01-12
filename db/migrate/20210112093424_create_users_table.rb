2708e6eeac1b367fc9e27c47832f0476a1db0786
Adi Skop <adiskop@gmail.com>
2021-01-12 14:45:14 +0100

    add timestamps to user table migration

--- db/migrate/20210112093424_create_users_table.rb
+++ db/migrate/20210112093424_create_users_table.rb
@@ -4,7 +4,9 @@ class CreateUsersTable < ActiveRecord::Migration
      t.string :name
      t.string :email
      t.string :password_digest
-     #bycrypt encrypted password
+     #Bcrypt stored salted, hashed version of out password as this db column
+     
+     t.timestamps null: false
     end
   end
 end
