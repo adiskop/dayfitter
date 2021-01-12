e8494826aed69f61a96d61583cf933c55193b374
Adi Skop <adiskop@gmail.com>
2021-01-12 14:42:50 +0100

    add has many association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActveRecord::Base
     has_secure_password
+    #allows us to use an activerecrd MACRO 
+    #(gives us access to few new method) called 'authinticate'
+    #which takes a paaword as a plain string and checks it against 
+    #Bcrypt hashing algorythm to make sure that it's the correct password. 
+
+    has_many :fitness_entries 
+
 end 
