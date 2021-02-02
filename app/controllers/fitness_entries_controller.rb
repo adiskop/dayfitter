90299fbbc20d8e34e01af0073858877c55cdfcda
Adi Skop <adiskop@gmail.com>
2021-02-02 11:27:48 -0800

    add post entries

--- app/controllers/fitness_entries_controller.rb
+++ app/controllers/fitness_entries_controller.rb
@@ -4,8 +4,22 @@ class FitnessEntriesController < ApplicationController
   get '/fitness_entries/new' do
     erb :'/fitness_entries/new'
   end
+
   # post fitness_entries to create a new fitness entry
   post '/fitness_entries' do
+    # I want to create a new fitness entry and save it to the database
+    # I only want to save the fitness entry if it has a user is logged in
+    if !logged_in?
+      redirect '/'
+    end
+    # I only want to save the fitness entry if it has any excersize list
+    if params[:excersize_list] !=""
+      # create a new fitness entry
+      @fitness_entry = FitnessEntry.create(excersize_list: params[:excersize_list], user_id: current_user.id)
+      redirect "/fitness_entries/#{@fitness_entry.id}"
+    else 
+      redirect '/fitness_entries/new'
+    end 
   end
   # show route for a fitness entry
 
