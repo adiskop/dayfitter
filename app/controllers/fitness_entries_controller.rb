9b537797cd168cef881b86173f20a6f2c94a96e9
Adi Skop <adiskop@gmail.com>
2021-02-03 14:37:03 -0800

    fix a few bugs

--- app/controllers/fitness_entries_controller.rb
+++ app/controllers/fitness_entries_controller.rb
@@ -23,17 +23,49 @@ class FitnessEntriesController < ApplicationController
   end
   # show route for a fitness entry
   get '/fitness_entries/:id' do
-    @fitness_entry = FitnessEntry.find(params[:id])
+    set_fitness_entry
     erb :'/fitness_entries/show'
   end
 
   # This route should send us to fitness_entries/edit.erb , which will 
   # render an edit form.   
   get '/fitness_entries/:id/edit' do
-    erb :'fitness_entries/edit'
+    set_fitness_entry
+    if logged_in?
+    if @fitness_entry.user == current_user
+      erb :'fitness_entries/edit'
+    else 
+      redirect "users/#{current_user.id}"
+    end
+  else
+    redirect '/'
+  end
+  end
 
+  # This action's job is to:   
+  patch '/fitness_entries/:id' do
+    # 1. find the fitness entry
+    set_fitness_entry
+    if logged_in?
+      if @fitness_entry.user == current_user
+    # 2. update the fitness entry
+    @fitness_entry.update(excersize_list: params[:excersize_list])
+    # 3. redirect to show page 
+    redirect "/fitness_entries/#{@fitness_entry.id}"
+      else 
+        redirect "users/#{current_user.id}"
+      end
+    else
+      redirect '/'
+    end
   end
 
 
   #index route for all fitness entries
+
+  private
+  def set_fitness_entry
+    @fitness_entry = FitnessEntry.find(params[:id])
+  end 
+
 end
--- config.ru
+++ config.ru
@@ -1,16 +1,16 @@
 #This is the file that allow us to build our project on top of Rack
 require './config/environment'
 
-if ActiveRecord::Migrator.needs_migration?
+if ActiveRecord::Base.connection.migration_context.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
 
-#in order to send PATCH and DELETE requests
+# in order to send PATCH and DELETE requests, I will need to add a line of code here..
 use Rack::MethodOverride
+
 
 #here is where i will mount other controllers with 'use'
 use UsersController
 use FitnessEntriesController
 run ApplicationController
 
-#run will run first
